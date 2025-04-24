efbab3136cea539b2055bc7b4fe569e7ec349189
Bunu <bunubhattarai39@gmail.com>
2025-04-24 13:10:56 -0400

    write your first C# code

---
+++ Introduction/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+// This is a simple C# console application that prints "Hello, World!" to the console.
+// It serves as a basic introduction to C# programming and the .NET environment.
+// It compiles your code into an executable format that a computer can understand.
+// It runs your compiled application and, when written correctly, will output "Hello World!"
+// To run this code, you can use the .NET CLI or an IDE like Visual Studio.
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("Your wrote your first line of C# code.");
+
+// Difference between Console.Write and Console.WriteLine:
+// Console.Write does not add a new line after the output, while Console.WriteLine does.
+// Console.WriteLine automatically appends a new line after the output, moving the cursor to the next line.
