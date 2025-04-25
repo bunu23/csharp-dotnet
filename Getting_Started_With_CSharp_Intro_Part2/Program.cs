168514c0d09fececc9bcd19a09bd8b8f22e2f31d
Bunu <bunubhattarai39@gmail.com>
2025-04-25 15:05:13 -0400

    getting started with array basics

--- Getting_Started_With_CSharp_Intro_Part2/Program.cs
+++ Getting_Started_With_CSharp_Intro_Part2/Program.cs
@@ -140,3 +140,4 @@ fraudulentOrderIDs[0] = "F000";
 Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
 
 Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
