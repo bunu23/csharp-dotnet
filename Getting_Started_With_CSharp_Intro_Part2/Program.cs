6010fc00cebcd6718ad9523be63202f17218a0d2
Bunu <bunubhattarai39@gmail.com>
2025-04-25 14:59:28 -0400

    getting started with array basics

--- Getting_Started_With_CSharp_Intro_Part2/Program.cs
+++ Getting_Started_With_CSharp_Intro_Part2/Program.cs
@@ -127,5 +127,16 @@ if (discountPercentage > 0)
 }
 
 //Store and Iterate Through Sequences of Data Using Arrays and the foreach Statement in C#
-// GETTING STARTED WITH ARRAYS
+// getting started with arrays
 
+string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
