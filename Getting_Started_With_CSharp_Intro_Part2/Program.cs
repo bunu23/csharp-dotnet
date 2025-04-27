91bfe90ec3f9c5020cd1e8c208e7435fc0064751
Bunu <bunubhattarai39@gmail.com>
2025-04-27 18:47:14 -0400

    challenge activity for nested iteration and selection statements

--- Getting_Started_With_CSharp_Intro_Part2/Program.cs
+++ Getting_Started_With_CSharp_Intro_Part2/Program.cs
@@ -166,3 +166,16 @@ foreach (int items in inventory)
 Console.WriteLine($"We have {sum} items in inventory.");
 
 
+/*
+Exercise - Complete a challenge activity for nested iteration and selection statements
+Code challenge - Your team has found a pattern. Orders that start with the letter "B" encounter fraud at a rate 25 times greater than the normal rate. You write new code that outputs the Order ID of new orders where the Order ID starts with the letter "B". This will be used by the fraud team to investigate further.
+*/
+
+string[] orderId = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+foreach (string order_ID in orderId)
+{
+    if (order_ID.StartsWith("B"))
+    {
+        Console.WriteLine(order_ID);
+    }
+}
